ada350b0c8bb5265290241cdb9d3f0d85aff905f
jef <e70838@gmail.com>
2022-12-09 18:21:11 +0100

    second commit

--- my_sokoban.c
+++ my_sokoban.c
@@ -1,6 +1,7 @@
 #include <ncurses.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h> 
 
 const int ERROR = 84;
 
@@ -13,6 +14,7 @@ const void usage() {
     "         ‘P’ for the player, ‘X’ for boxes and ‘O’ for storage locations.\n");
 }
 
+/* returns a newly allocated char * containing the content of the file */
 char * read_map(const char * filename) {
     long length;
     char * res;
@@ -34,8 +36,87 @@ char * read_map(const char * filename) {
     return res;
 }
 
+typedef struct sokoban_map {
+    const char * init; /* initial state shall be preserved to allow walking over "O" */
+    char * m;
+    size_t w, h; /* width and height */
+} * SokobanMap;
+
+typedef struct position {
+    size_t x, y, pos;
+} * Position;
+
+SokobanMap make_map_from_string (const char * m) {
+    SokobanMap res = (SokobanMap)malloc(sizeof(struct sokoban_map));
+    char * ptr;
+    res->init = strdup(m);
+    res->m = strdup(m);
+    /* remove the player from the reference map */
+    ptr = strchr(res->init, 'P');
+    *ptr = ' ';
+    /* count width */
+    ptr = strchr(m, '\n');
+    do {
+        ptr++;
+    } while (*ptr != '#');
+    res->w = ptr-m;
+    res->h = 0;
+}
+
+Position get_player_position(SokobanMap m) {
+    Position res = (Position)malloc(sizeof(struct position));
+    char * ptr = strchr(m->m, 'P');
+    res->pos = ptr-m->m;
+    res->x = res->pos % m->w;
+    res->y = (res->pos - res->x) / m->w;
+    return res;
+}
+
+bool isGameFinished(SokobanMap m) {
+    const char * po = m->init; /* next storage location */
+    do {
+        po = strchr(po, 'O');
+        if (m->m[po-m->init] != 'X') {
+            return false;
+        }
+    } while (po++);
+    return true;
+}
+
+void update_map(SokobanMap m, int ch) {
+    int dx = 0;
+    int dy = 0;
+    int new_x;
+    int new_y;
+    int new_pos;
+    Position p = get_player_position(m);
+    if (ch == KEY_LEFT) {
+        dx = -1;
+	} else if (ch == KEY_RIGHT) {
+        dx = 1;
+	} else if (ch == KEY_UP) {
+        dy = -1;
+	} else if (ch == KEY_DOWN) {
+        dy = +1;
+    } else {
+        return;
+    }
+    new_x = p->x + dx;
+    new_y = p->y + dy;
+    if (new_x < 0 || new_y <0 || new_x >= m->w || new_y >= m->h) {
+        return;
+    }
+    new_pos = new_x + new_y * m->w;
+    if (m->m[new_pos] == ' ') {
+        m->m[new_pos] = 'P';
+        m->m[p->pos] = m->init[p->pos];
+    }
+}
+
 int main(int argc, char * argv[]) {
-    const char * map;
+    const char * map_string;
+    SokobanMap m;
+    bool finished = false;
 
     if (argc != 2) {
         usage();
@@ -45,7 +126,24 @@ int main(int argc, char * argv[]) {
         return 0;
     }
 
-    map = read_map(argv[1]);
-    printf("%s", map);
+    map_string = read_map(argv[1]);
+    m = make_map_from_string (map_string);
+    free((void *)map_string);
+
+    initscr();
+    raw();
+    keypad(stdscr, true);
+    do {
+        mvprintw(0, 0, "%s", m->m);
+        refresh();
+        int ch = getch();
+        if (ch == 'r' || ch == 'R') { /* resign */
+            finished = true;
+        } else {
+            update_map(m, ch);
+            /* todo check if won */
+        }
+	} while (!finished);
+    endwin();
     return 0;
 }
